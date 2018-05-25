f5528f422bb6a1a3cad3489ec5e93b1088a00209
manhquan01 <manhquan1908@gmail.com>
2018-05-25 21:38:01 +0700

    permission

--- index.php
+++ index.php
@@ -50,10 +50,9 @@ if (isset($_POST["submit"])) {
 
   <body>
 
-    <div class="row">
+    <div class="row" style="margin: 0;">
       <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
         <div class="login-panel panel panel-default">
-         <!--  <?php echo $rows; if(isset($alert)){echo $alert;} ?> -->
           <div class="panel-heading">Đăng nhập hệ thống quản trị</div>
           <div class="panel-body">
             <?php
